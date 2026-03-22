cd1337e04b11d2067f0ed9f96f2d90832f409031
Aryan Aggrawal <aryanaggrawal@Aryans-MacBook.local>
2026-03-22 17:45:35 +0530

    Fix ENETUNREACH error by removing service property in transporter

--- BACKEND/utils/sendEmail.js
+++ BACKEND/utils/sendEmail.js
@@ -4,8 +4,7 @@ export const sendEmail = async(options)=>{
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: Number(process.env.SMTP_PORT),
-        service: process.env.SMTP_SERVICE,
-        secure: Number(process.env.SMTP_PORT) === 465, // true for 465, false for other ports
+        secure: Number(process.env.SMTP_PORT) === 465,
         auth: {
             user: process.env.SMTP_MAIL,
             pass: process.env.SMTP_PASSWORD,
